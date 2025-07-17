cd47028a458b24dde5e8107180b9c47f5aa33510
dk-110 <diyazara110@gmail.com>
2025-07-17 05:11:05 +0500

    First version

---
+++ temp_code.R
@@ -0,0 +1,7 @@
+# install.packages("AER")
+library(AER)
+library(tidyverse)
+help(package = "AER")
+data("ResumeNames")
+x <- as_tibble(ResumeNames) |> 
+  select(name, gender, ethnicity, quality, call, city, experience)
